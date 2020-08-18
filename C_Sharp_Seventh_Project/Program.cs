40638c2022ded66971b428a0cbe133a0cac06a4e
FarjanaFerdousiTamanna <farjana.ferdousi.tamanna@gmail.com>
2020-08-18 16:56:16 +0600

    Add project files.

---
+++ C_Sharp_Seventh_Project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace C_Sharp_Seventh_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+
+
+            if (num == 1)
+            {
+                Console.WriteLine("The number is 1");
+            }
+
+            else if (num == 3)
+            {
+                Console.WriteLine("The number is 3");
+            }
+
+            else
+            {
+                Console.WriteLine("The number is 10");
+            }
+        }
+    }
+}
